6d058a57bfdce509ece58e588b32eb72e889f372
DharmikNarola111 <hellonarola@gmail.com>
2023-05-06 00:51:09 -0400

    Basic layout created

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { createMuiTheme } from '@mui/material';
+import { themeSettings } from 'theme';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Navigator, Route, Routes } from 'react-router-dom';
+import { RuleTwoTone } from '@mui/icons-material';
+import {useMemo} from "react";
+import Layout from "scenes/Layout";
+import Dashboard from 'scenes/dashboard';
+import { Navigate } from 'react-router-dom';
+
+
+function App() {
+
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(()=>createTheme(themeSettings(mode)),[mode]);
+  return (
+    <div className="App">
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Navigate to="/dashboard" replace/>} />
+            <Route path="/dashboard" element={<Dashboard/>} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { LightModeOutlined, DarkModeOutlined, Menu as MenuIcon, Search, SettingsOutlined, ArrowDropDownOutlined } from '@mui/icons-material';
+import FlexBetween from 'components/FlexBetween';
+import { useDispatch } from 'react-redux';
+import { setMode } from 'state';
+import profileImage from "assets/man.jpg"
+
+
+const navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbar
